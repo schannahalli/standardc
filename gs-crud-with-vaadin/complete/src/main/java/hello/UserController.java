ef495c3e9558b65b9ca518a1518c0648c0a30e66
schannahalli <schannahalli@bitbucket.org>
2019-02-25 00:46:52 -0800

    few more checks

--- gs-crud-with-vaadin/complete/src/main/java/hello/UserController.java
+++ gs-crud-with-vaadin/complete/src/main/java/hello/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
 public class UserController {
@@ -22,11 +24,35 @@ public class UserController {
 
         Response resp = new Response();
 
+        Boolean isUserInfoValid = true ;
+        if(userInfo.getEmailId() == null || userInfo.getPassword() == null || userInfo.getUserName() == null){
+            isUserInfoValid = false ;
+        }
+
+        if(userInfo.getUserName().length() == 0 || userInfo.getPassword().length() == 0 || userInfo.getEmailId().length() == 0){
+            isUserInfoValid = false ;
+        }
+
+        //Check if valid email
+        Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+        Matcher mat = pattern.matcher(userInfo.getEmailId());
+
+        if(!mat.matches()){
+            isUserInfoValid = false ;
+        }
+
+        if(!isUserInfoValid) {
+            resp.errorMessage = Constants.INCORRECT_USER_INFO;
+            resp.errorCode = Constants.INCORRECT_USER_INFO_EC;
+            return resp;
+        }
+
         Boolean ret = userService.signup(userInfo);
 
         if(ret) {
             resp.errorCode = 0L;
             resp.errorMessage = "success";
+            return resp ;
         }
 
         resp.errorCode = 101L ;
@@ -35,10 +61,29 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    @ResponseBody Response loginWithEmail(@RequestBody UserInfoDTO userInfoDTO){
+    public @ResponseBody Response login(@RequestBody UserInfoDTO userInfoDTO){
         Response response = new Response();
+        Boolean isValidUserInfo = true ;
+        if((userInfoDTO.getEmailId() == null || userInfoDTO.getEmailId().length() == 0)
+                && ((userInfoDTO.getUserName() == null || userInfoDTO.getUserName().length() == 0))){
+            isValidUserInfo = false ;
+        }
+        if(userInfoDTO.getPassword() == null || userInfoDTO.getPassword().length() == 0){
+            isValidUserInfo = false ;
+        }
+
+        if(!isValidUserInfo){
+            response.errorMessage = Constants.INCORRECT_USER_INFO;
+            response.errorCode = Constants.INCORRECT_USER_INFO_EC;
+            return response;
+        }
+
         try{
-            userService.loginWithEmail(userInfoDTO);
+            if(userInfoDTO.getUserName()==null || userInfoDTO.getUserName().length()==0) {
+                userService.loginWithEmail(userInfoDTO);
+            }else{
+                userService.loginWithUserName(userInfoDTO);
+            }
         }catch(CustomException e){
             response.errorCode = e.getErrorCode() ;
             response.errorMessage = e.getErrorMessage();
